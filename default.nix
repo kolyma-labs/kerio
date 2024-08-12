78c4b7b17522db757c61b49216ad6c78d3a4abb8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-12 17:11:55 +0500

    uploading final result

---
+++ default.nix
@@ -0,0 +1,48 @@
+{ pkgs ? import <nixpkgs> { }, stdenv }:
+let
+  lib = pkgs.lib;
+  version = "9.4.4-8434";
+in
+stdenv.mkDerivation rec {
+  pname = "kerio-control-vpnclient";
+  version = version;
+
+  src = fetchurl {
+    url = "https://cdn.kerio.com/dwn/control/control-${version}/${pname}-${version}-linux-amd64.deb";
+    # sha256sum kerio-control-vpnclient-9.4.4-8434-linux-amd64.deb
+    # nix hash to-sri --type sha256 ad04c68c2af9928534ea769f2bf2d6aa7784742d24658616c1399407b31783f8
+    hash = "sha256-rQTGjCr5koU06nafK/LWqneEdC0kZYYWwTmUB7MXg/g=";
+  };
+
+  nativeBuildInputs = [
+    dpkg
+  ];
+
+  buildInputs = [
+  ];
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r * $out/
+  '';
+
+  meta = with lib; {
+    homepage = "http://www.kerio.com/control";
+    description = "Kerio Control VPN client for corporate networks.";
+    licencse = licenses.mit;
+    platforms = with platforms; linux;
+
+    maintainers = [
+      {
+        name = "Sokhibjon Orzikulov";
+        email = "sakhib@orzklv.uz";
+        handle = "orzklv";
+        github = "orzklv";
+        githubId = 54666588;
+        keys = [{
+          fingerprint = "00D2 7BC6 8707 0683 FBB9  137C 3C35 D3AF 0DA1 D6A8";
+        }];
+      }
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,66 +1,55 @@
 {
-  description = "Kerio connnect for NixOS";
+  description = "Kerio Control VPN flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    packages.${system}.default = self.packages.x86_64-linux.kerio;
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    } @ inputs:
+    let
+      lib = nixpkgs.lib;
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-    packages.${system}.kerio = pkgs.stdenv.mkDerivation rec {
-        pname = "kerio-control-vpnclient";
-        version = "9.4.2.7290";
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
 
-        src = pkgs.fetchurl {
-          url = "https://cdn.kerio.com/dwn/control/control-${version}-${version}/kerio-control-vpnclient-${version%.*}-${version}-p1-linux-amd64.deb";
-          sha256 = "019e015b72196b5e16c26ff6f2b365b0ddefd158520fbbd0a30a738ca7061b41";
-        };
+      pkgsFor = lib.genAttrs systems (system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        });
 
-        nativeBuildInputs = [ pkgs.libarchive ];
+      devShellFor = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          script = import ./shell.nix { inherit pkgs; };
+        in
+        script;
+    in
+    {
+      # Nix script formatter
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
 
-        buildInputs = [ pkgs.procps pkgs.dialog pkgs.util-linux pkgs.libxcrypt_compat ];
+      # Development environment
+      devShells = lib.mapAttrs (system: _: devShellFor system) (lib.genAttrs systems { });
 
-        installPhase = ''
-          bsdtar -xf $src -C $out
-
-          install -m 755 -d $out/usr/bin
-          install -m 755 -t $out/usr/bin $out/usr/sbin/kvpncsvc
-
-          # Install the main binary
-
-          # Install additional libraries
-          install -m 755 -d $out/usr/lib/${pname}
-          install -m 755 -t $out/usr/lib/${pname} $out/usr/lib/*
-
-          # Install documentation
-          install -m 755 -d $out/usr/share/doc/${pname}
-          install -m 644 -t $out/usr/share/doc/${pname} $out/usr/share/doc/${pname}/Acknowledgments.gz
-          install -m 644 -t $out/usr/share/doc/${pname} $out/usr/share/doc/${pname}/copyright
-
-          # Install configuration file
-          install -m 644 ${./kvpnc.conf} $out/etc/conf.d/kvpnc.conf
-
-          # Install systemd service
-          install -m 644 ${./kvpnc.service} $out/usr/lib/systemd/system/kvpnc.service
-
-          # Install license file
-          gzip -dfc $out/usr/share/doc/${pname}/EULA.txt.gz > $out/usr/share/licenses/${pname}/EULA.txt
-            '';
-
-        meta = with pkgs.lib; {
-          description = "Kerio Control VPN client for corporate networks.";
-          homepage = "http://www.kerio.com/control";
-          license = licenses.unfreeRedistributable;
-          platforms = platforms.linux;
-          maintainers = with maintainers; [ yourname ];
-        };
+      # Output package
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./. { };
+      });
     };
-  };
 }
