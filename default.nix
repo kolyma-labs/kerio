c2bbfbeafc427481b98d0856c8860840142d7573
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-12 17:14:43 +0500

    buildable with pkgs.fetchUrl

--- default.nix
+++ default.nix
@@ -1,20 +1,19 @@
 { pkgs ? import <nixpkgs> { }, stdenv }:
 let
   lib = pkgs.lib;
-  version = "9.4.4-8434";
 in
 stdenv.mkDerivation rec {
   pname = "kerio-control-vpnclient";
-  version = version;
+  version = "9.4.4-8434";
 
-  src = fetchurl {
+  src = pkgs.fetchurl {
     url = "https://cdn.kerio.com/dwn/control/control-${version}/${pname}-${version}-linux-amd64.deb";
     # sha256sum kerio-control-vpnclient-9.4.4-8434-linux-amd64.deb
     # nix hash to-sri --type sha256 ad04c68c2af9928534ea769f2bf2d6aa7784742d24658616c1399407b31783f8
     hash = "sha256-rQTGjCr5koU06nafK/LWqneEdC0kZYYWwTmUB7MXg/g=";
   };
 
-  nativeBuildInputs = [
+  nativeBuildInputs = with pkgs; [
     dpkg
   ];
 
